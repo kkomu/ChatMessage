ff9d5632d3167129a67a02993a353623ae6e2094
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame3-THINK>
2014-11-27 14:32:49 +0200

    initial commit

---
+++ src/message/ChatMessage.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+import java.io.Serializable;
+
+/**
+ * This class can be used as serializable object between the client and server
+ * in chat application
+ * 
+ * @author Kyo Komu
+ * @version 0.0.1
+ */
+public class ChatMessage implements Serializable {
+    private String userName;
+    private String chatMessage;
+    private String messageColor;
+    private int fontSize;
+    private boolean isPrivate;
+    private String privateName;
+    private boolean nameUpdate;
+    private boolean userListUpdate;
+
+    public boolean isUserListUpdate() {
+        return userListUpdate;
+    }
+
+    public void setUserListUpdate(boolean userListUpdate) {
+        this.userListUpdate = userListUpdate;
+    }
+    
+    public boolean isNameUpdate() {
+        return nameUpdate;
+    }
+
+    public void setNameUpdate(boolean nameUpdate) {
+        this.nameUpdate = nameUpdate;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getChatMessage() {
+        return chatMessage;
+    }
+
+    public void setChatMessage(String chatMessage) {
+        this.chatMessage = chatMessage;
+    }
+
+    public String getMessageColor() {
+        return messageColor;
+    }
+
+    public void setMessageColor(String messageColor) {
+        this.messageColor = messageColor;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public boolean isIsPrivate() {
+        return isPrivate;
+    }
+
+    public void setIsPrivate(boolean isPrivate) {
+        this.isPrivate = isPrivate;
+    }
+
+    public String getPrivateName() {
+        return privateName;
+    }
+
+    public void setPrivateName(String privateName) {
+        this.privateName = privateName;
+    }
+    
+    
+}
